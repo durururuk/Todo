bbc9a1b72fe0afba0d33661e626ddcef86877a96
durururuk <zkdltms0913@gmail.com>
2024-08-14 13:48:16 +0900

    git init

---
+++ src/main/java/com/sparta/todore/TodoReApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.todore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoReApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoReApplication.class, args);
+    }
+
+}
